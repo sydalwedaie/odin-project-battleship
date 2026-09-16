f92e677c7deafbf31a839fb6c54a6ecf34b251d2
Sayed Alwedaie <5216479+sydalwedaie@users.noreply.github.com>
2026-09-17 00:36:23 +0300

    remove winning game test from index.js

--- src/index.js
+++ src/index.js
@@ -5,34 +5,4 @@ import "./template.html";
 import { ConsoleController } from "./scripts/controller/console.js";
 
 const consoleController = ConsoleController();
-
-function testWin() {
-  const targets = [
-    [1, 1],
-    [8, 1],
-    [1, 2],
-    [5, 2],
-    [8, 2],
-    [5, 3],
-    [8, 3],
-    [5, 4],
-    [5, 5],
-    [4, 7],
-    [5, 7],
-    [6, 7],
-    [7, 7],
-    [8, 7],
-    [1, 8],
-    [2, 8],
-    [3, 8],
-  ];
-
-  targets.forEach((target) => {
-    consoleController.play(target);
-    consoleController.play(target);
-  });
-}
-
-testWin();
-
 window.consoleController = consoleController;
