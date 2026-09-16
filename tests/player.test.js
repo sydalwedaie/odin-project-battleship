7a7b93753a5721dee7bce71f608aa5d40eacffd1
Sayed Alwedaie <5216479+sydalwedaie@users.noreply.github.com>
2026-09-16 21:21:23 +0300

    implement tests for Player

---
+++ tests/player.test.js
@@ -0,0 +1,38 @@
+import { describe, expect, test } from "@jest/globals";
+import { Player } from "../src/scripts/game/player.js";
+
+describe("Test factory: Player", () => {
+  const player = Player("name");
+
+  test("should return name of player", () => {
+    expect(player.name).toBe("name");
+  });
+
+  test("should throw error if number of positions is not exactly 5", () => {
+    expect(() => {
+      player.placeShips([[1, 1, "h"], [(2, 2, "h")]]);
+    }).toThrow();
+    expect(() => {
+      player.placeShips([
+        [1, 1, "h"],
+        [2, 2, "h"],
+        [3, 3, "h"],
+        [4, 4, "h"],
+        [5, 5, "h"],
+        [6, 6, "h"],
+      ]);
+    }).toThrow();
+  });
+
+  test("should place 5 ships", () => {
+    player.placeShips([
+      [4, 7, "v"],
+      [5, 2, "h"],
+      [8, 1, "h"],
+      [1, 8, "v"],
+      [1, 1, "h"],
+    ]);
+
+    expect(player.gameboard.fleet.length).toBe(5);
+  });
+});
