6ee660d71a7581eed62a0bf88054ae1769642947
Sayed Alwedaie <5216479+sydalwedaie@users.noreply.github.com>
2026-09-18 11:42:17 +0300

    implement a random ship placement method in Player
    Experiment with doing random placement directly in the Player module.

--- src/scripts/game/player.js
+++ src/scripts/game/player.js
@@ -3,19 +3,18 @@ import { Ship } from "./ship.js";
 
 export function Player(name) {
   const gameboard = Gameboard();
+  const ships = [
+    [5, "Carrier"],
+    [4, "Battleship"],
+    [3, "Destroyer"],
+    [3, "Submarine"],
+    [2, "Patrol Boat"],
+  ];
 
   const placeShips = (positions) => {
     if (positions.length !== 5) {
       throw new Error("number of positions is not exactly 5");
     }
-
-    const ships = [
-      [5, "Carrier"],
-      [4, "Battleship"],
-      [3, "Destroyer"],
-      [3, "Submarine"],
-      [2, "Patrol Boat"],
-    ];
 
     positions.forEach((pos, index) => {
       const [length, name] = ships[index];
@@ -23,5 +22,25 @@ export function Player(name) {
     });
   };
 
-  return { name, gameboard, placeShips };
+  const placeShipsRandom = () => {
+    ships.forEach(([length, name], index) => {
+      while (gameboard.fleet.length === index) {
+        const row = Math.floor(Math.random() * 10);
+        const col = Math.floor(Math.random() * 10);
+        const orientation = ["h", "v"][Math.floor(Math.random() * 2)];
+        if (
+          (orientation === "h") & (col + length < 10) ||
+          (orientation === "v") & (row + length < 10)
+        ) {
+          try {
+            gameboard.placeShip(Ship(length, name), [row, col, orientation]);
+          } catch (e) {
+            console.log(e);
+          }
+        }
+      }
+    });
+  };
+
+  return { name, gameboard, placeShips, placeShipsRandom };
 }
